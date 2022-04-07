1fa043433002fd1ccc6edfa03fe7a9902bb33fd5
Sultan Abu Baker <87023622+sultanabubaker@users.noreply.github.com>
2022-04-07 10:31:14 +0300

    Update main.java

--- main.java
+++ main.java
@@ -4,14 +4,5 @@ import java.io.File;
 
 class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		if (args.length > 0) {
-			String[] files = new File(args[0]).list();
-			if (files != null) {
-			    System.out.println("Len: " + files.length);
-			} else {
-			    System.out.println("null");
-			}
-		     }
 		}
 	   }
